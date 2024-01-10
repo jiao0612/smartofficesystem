5bb4f43a3e67f707f98c720003e6d97583cf9966
jzm <jj20191202@163.com>
2024-01-10 22:15:37 +0800

    配置日志

--- filsystem/src/main/java/com/itjzmproject/filsystem/FilsystemApplication.java
+++ filsystem/src/main/java/com/itjzmproject/filsystem/FilsystemApplication.java
@@ -1,16 +1,21 @@
 package com.itjzmproject.filsystem;
 
+import com.itjzmproject.filsystem.common.util.LoggerUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FilsystemApplication {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(FilsystemApplication.class);
+
     public static void main(String[] args) {
         try {
             SpringApplication.run(FilsystemApplication.class, args);
         } catch (Error error) {
-            System.out.println("start application file! please see error log!");
+            LoggerUtil.error(LOGGER, error, "start application file! please see error log!");
         }
     }
 
--- filsystem/src/main/java/com/itjzmproject/filsystem/file/FileManagerController.java
+++ filsystem/src/main/java/com/itjzmproject/filsystem/file/FileManagerController.java
@@ -1,10 +1,14 @@
 package com.itjzmproject.filsystem.file;
 
+import com.itjzmproject.filsystem.FilsystemApplication;
+import com.itjzmproject.filsystem.common.util.LoggerUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @Description: TODO
+ * @Description: 文件管理
  * @Author: jzm
  * @CreateTime: 2024-01-08  21:24
  * @Version: 1.0
@@ -13,9 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/file/")
 public class FileManagerController {
 
+    /**
+     * logger
+     */
+    private final static Logger LOGGER = LoggerFactory.getLogger(FileManagerController.class);
+
+
     @RequestMapping("getFileName")
     public String getFileName(){
-        System.out.println("true = " + true);
+        LoggerUtil.info(LOGGER, "【文件管理系统】获取文件名称");
         return "xx";
     }
 }
